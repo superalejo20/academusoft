7989b9e6f79af6aa00e8e112c3ce3795aacf603a
superalejo20 <superalejo20@hotmail.com>
2019-05-30 14:50:57 -0500

    comit3

--- ModuloEscritorio/src/main/java/controladores/RegistrarEstudianteController.java
+++ ModuloEscritorio/src/main/java/controladores/RegistrarEstudianteController.java
@@ -1,45 +1,15 @@
 package controladores;
 
-import java.awt.HeadlessException;
-
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.ResourceBundle;
-
-import javax.swing.JOptionPane;
-
-
-import javafx.fxml.Initializable;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyCombination;
-import javafx.stage.Stage;
-import javafx.util.StringConverter;
 
-public class RegistrarEstudianteController implements Initializable{
-	
-	RegistrarEstudianteController registrarCliente;
+public class RegistrarEstudianteController {
 
-
-    
     @FXML
     private TextField txtNombre;
 
@@ -68,11 +38,11 @@ public class RegistrarEstudianteController implements Initializable{
     private Button btnRegistrar;
 
     @FXML
-    private ComboBox<String> tipoPrestamo;
+    private ComboBox<?> tipoPrestamo;
 
     @FXML
     private Label lblTipoP;
-    
+
     @FXML
     private Label lblInformacion;
 
@@ -83,7 +53,7 @@ public class RegistrarEstudianteController implements Initializable{
     private TextField txtValorP;
 
     @FXML
-    private ComboBox<Integer> cuotasDiferidas;
+    private ComboBox<?> cuotasDiferidas;
 
     @FXML
     private Label lblCuotasDiferidas;
@@ -98,79 +68,31 @@ public class RegistrarEstudianteController implements Initializable{
     private TextField txtPropietario1;
 
     @FXML
-    private Label lbIdentificacion;
-
-    @FXML
-    private TextField txtCedula1;
-
-    @FXML
-    private Label lblTasaInteres;
-
-    @FXML
-    private TextField txtInteres;
-
-    @FXML
     private DatePicker fechaSeleccionada;
 
     @FXML
-    private Label lblValorCuotas;
-
-    @FXML
-    private TextField txtValorCuotas;
-
-    @FXML
-    private Label lblTotalPrestamo;
-
-    @FXML
-    private TextField txtTotalPrestamo;
-
-    /**
-     * @param event
-     */
-    @FXML
-    void registrarCliente(ActionEvent event) {
-
+    void calcularCuotas(ActionEvent event) {
 
     }
-	    
 
-	@FXML
-    void volver(ActionEvent event) {
-
-    }
-	    
-	    
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		
-		
-	}
-
-	@FXML
+    @FXML
     void fechaSeleccionada(ActionEvent event) {
 
     }
-    
-    
-    @FXML
-    public void calcularCuotas(ActionEvent event) {
-    	
-    }
 
+    @FXML
+    void registrarCliente(ActionEvent event) {
+
+    }
 
     @FXML
     void tipoPrestamo(ActionEvent event) {
-    	
+
     }
-	    
+
+    @FXML
+    void volver(ActionEvent event) {
+
+    }
+
 }
-
-	   
-
-
-
-
-
-
-	
